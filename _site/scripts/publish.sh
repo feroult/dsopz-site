210d113a5c126090e62594aa179c5b0a10a43394
feroult <feroult@gmail.com>
2016-12-06 22:09:01 -0200

    publishing

---
+++ _site/scripts/publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+jekyll b
+#sed -i '' -e 's/\.html//g' _site/sitemap.xml
+#sed -i '' -e 's/\<loc\>/\<loc\>http:\/\/yawp.io/g' _site/sitemap.xml
+git add .
+git commit -am "publishing"
+git push
+git subtree push --prefix _site origin gh-pages
